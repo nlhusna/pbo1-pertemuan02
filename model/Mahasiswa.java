26cb9d8af6ca82255e9da54c46d465221060a991
nlhusna <nidaulhusna48@gmail.com>
2024-03-30 14:11:33 +0800

    Memanggil method dari banyak Class di Package lain

---
+++ model/Mahasiswa.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Mahasiswa{
+ public static void tampilanInfo(){
+  System.out.println();
+  System.out.println("== Data Mahasiswa ==================");
+  System.out.println("NPM : 2210010706");
+  System.out.println("Nama : Nidaul Husna");
+  System.out.println("====================================");
+ }
+}
