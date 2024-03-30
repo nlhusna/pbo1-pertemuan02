1ba51a95052323048edff4fe04db5b0a7931f136
nlhusna <nidaulhusna48@gmail.com>
2024-03-30 15:17:39 +0800

    Memanggil method dari banyak Class di Package lain

--- InputOuput.java
+++ InputOuput.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class InputOutput{
- public static void main(String[] args){
+ public static void Main(String[] args){
   Scanner scanner = new Scanner(System.in)
   String nama;
   System.out.print("Masukkan nama anda: ");
